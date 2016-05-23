fcdd4657f9ba9336c79d141df5086f67e0dcc26c
slebreault <samuellebreault@yahoo.com>
2016-05-23 11:39:40 -0400

    Update form.js

--- form.js
+++ form.js
@@ -258,6 +258,18 @@ function generateWhere() {
 }
 */
 
+function init(){		
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+	          acc[i].onclick = function(){
+		this.classList.toggle("active");
+	          this.nextElementSibling.classList.toggle("show");
+		}
+	}
+}
+		
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
